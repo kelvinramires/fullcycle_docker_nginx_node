d426c6e22c9219e47f3b2d15945089dbc74db32b
Kelvin Ramires Capobianco <kelvinramires@gmail.com>
2022-04-16 00:51:33 -0300

    feat(docker): adding challenge code

---
+++ README.md
@@ -0,0 +1,3 @@
+# FullCycle Challange for Docker Module with node + nginx + mysql
+
+To run: `docker-compose up -d`
---
+++ node/index.js
@@ -0,0 +1,29 @@
+const express = require("express")
+const app = express()
+const port = 3000
+const config = {
+    host: 'db',
+    user: 'root',
+    password: 'teste',
+    database: 'nodedb'
+};
+const mysql = require('mysql')
+const connection = mysql.createConnection(config)
+
+app.get('/', (req,res) => {
+    const number = Math.floor(Math.random() * (1000)) + 1
+    const nome = 'Kelvin ' + number
+    let sql = `INSERT INTO people(name) values ('${nome}')`
+    connection.query(sql)
+    connection.query(`SELECT name FROM people`, (error, results) => {
+        res.send(`
+      <h1>Full Cycle Rocks!</h1>
+      <ol>
+        ${!!results.length ? results.map(el => `<li>${el.name}</li>`).join('') : ''}
+      </ol>
+    `)
+    })
+})
+app.listen(port, () => {
+    console.log('Rodando na porta ' + port)
+})
